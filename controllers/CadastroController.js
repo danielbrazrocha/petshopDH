7acc92d26d57795f1fbfee89841438c786a284ec
Daniel Rocha <dgustavo@id.uff.br>
2021-09-23 16:47:00 -0300

    add Cadastro Controller

--- controllers/CadastroController.js
+++ controllers/CadastroController.js
@@ -1,9 +1,9 @@
-const LoginController =  {
+const CadastroController =  {
     // index = método do controller para renderizar uma view, chamado em index.js
     index: (req, res) => {
         // indica o arquivo EJS dentro de view a ser chamado
-        return res.render('login');
+        return res.render('cadastro');
     }
 }
 
-module.exports = LoginController;
+module.exports = CadastroController;
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var ContatoController = require('../controllers/ContatoController');
 var LoginController = require('../controllers/LoginController');
+var CadastroController = require('../controllers/CadastroController');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -13,5 +14,8 @@ router.get('/contato', ContatoController.index);
 
 // View Login
 router.get('/login', LoginController.index);
+
+// View Cadastro
+router.get('/cadastro', CadastroController.index);
 
 module.exports = router;
